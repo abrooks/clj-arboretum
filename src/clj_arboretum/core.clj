a1d3e051a798c6d7a4d4e47bc97cfc70e7a4b325
Aaron Brooks <aaron@brooks1.net>
2012-05-24 12:34:27 -0400

    Updates from last night and discussion with Joel and Chouser.

--- src/clj_arboretum/core.clj
+++ src/clj_arboretum/core.clj
@@ -1,53 +1,52 @@
 (ns clj-arboretum.core)
 
-(defn plant
-  [c]
-  (if (map? c)
-    c
-    (map-indexed vector c)))
-
-(defn climb
+(defn- climb
   [so-far branch roots]
-  (let [mso-far (:subtrees (meta so-far))
+  (let [m-so-far (:subtrees (meta so-far))
         vconj (fnil conj [])]
     (with-meta (vconj so-far branch)
-      {:subtrees (vconj mso-far roots)} )))
+      {:subtrees (vconj m-so-far roots)} )))
 
-(defn leaf
+(defn leaves
   "Climb a tree of nested associative data structures, returning their
-  leaf nodes in as map values, indexed by a vector of the keys to
-  reach those leaf values."
+  leaf nodes in as tuples of vector-of-path-elements and value. e.g.:
+  (leaves {:a {:b {:c 1 :d 2}}})
+  '([[:a :b :c] 1]
+    [[:a :b :d] 2]
+    ...) "
   [roots & [so-far]]
   (if (associative? roots)
-    (for [[trunk branches] (plant roots)
-          leaves (leaf branches (climb so-far trunk roots))]
-      leaves)
+    (for [[trunk branches] (cond (map? roots) roots
+                                 :else (map-indexed vector roots))
+          leaf (leaves branches (climb so-far trunk roots))]
+      leaf)
     [[so-far roots]]))
 
 (defn mulch
   "Analyze and throw helpful exception."
-  [e leaves]
+  [e roots leaves]
   (throw e))
 
-(defn grow
+(defn- grow
   [roots [branches leaf]]
   (if branches
     (let [[trunk & nbranches] branches
           [m-trunk & m-nbranches] (:subtrees (meta branches))
           nbranches (when nbranches
-                  (with-meta nbranches {:subtrees m-nbranches}))
+                      (with-meta nbranches {:subtrees m-nbranches}))
           roots (or roots (empty m-trunk))
           nroots (get roots trunk)]
       (assoc roots trunk (grow nroots [nbranches leaf])))
     leaf))
 
 (defn tree
-  [leaves]
-  (try
-    (loop [roots nil leaves leaves]
-      (if (seq leaves)
-        (let [roots (grow roots (first leaves))]
-          (recur roots (rest leaves)))
-        roots))
-    (catch Exception e
-      (mulch e leaves))))
+  ([leaves] (tree nil leaves))
+  ([roots leaves]
+     (try-debug mulch
+       (loop [roots roots leaves leaves]
+         (if (seq leaves)
+           (let [roots (grow roots (first leaves))]
+             (recur roots (rest leaves)))
+           roots)))))
+
+
